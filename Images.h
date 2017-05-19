79f5c51cf84d945ee48cc949329e7fb4c3b82bbb
Rubikoid <dimazotoff8@gmail.com>
2017-05-19 17:12:37 +0300

    Tank moving

--- Images.h
+++ Images.h
@@ -22,16 +22,18 @@ class ImageSheet {
 
     ImageSheet(char* imageName, int deltaX, int deltaY, int sizeX, int sizeY) {
         image = loadImage(imageName);
-        dX = deltaX;
+        dX = deltaX; //size of one
         dY = deltaY;
-        posX = 0;
+        posX = 0; //pos in pic
         posY = 0;
-        nX = sizeX;
+        nX = sizeX; //size of pic
         nY = sizeY;
     }
 
-    void draw(int x, int y, int xMove = 0, int yMove = 0) {
-        txTransparentBlt(getDC(), x, y, dX, dY, image, (posX+xMove)*dX, (posY+yMove)*dY, RGB(0,96,64));
+    void draw(double x, double y, int xMove = -1, int yMove = -1) {
+		xMove = xMove == -1 ? posX : xMove;
+		yMove = yMove == -1 ? posY : yMove;
+        txTransparentBlt(getDC(), x, y, dX, dY, image, xMove*dX, yMove*dY, RGB(0,96,64));
     }
 
     void moveRight() {
@@ -44,17 +46,14 @@ class ImageSheet {
         if(posX < 0) posX = nX/dX - 1;
     }
 
-    float mouseMagic(float x, float y) {
-        char a[100];
-        float f = 0.0;
+    double mouseMagic(double x, double y) {
+        double f = 0.0;
         x+= dX/2.0;
         y+= dY/2.0;
         f = atan2((MouseY() - y)*-1,MouseX() - x)*180/M_PI;
 		f = 90 - f;
 		if (f < 0) f = (90 - f*-1) + 270;
-		sprintf(a, "%f;%f;%f", MouseX() - x, MouseY() - y, f);
-		txLine((double)x,(double)y,MouseX(),MouseY());
-		textOut(1000, 50, a);
+		txLine(x,y,MouseX(),MouseY());
 		return f;
 	}
 
--- Main.cpp
+++ Main.cpp
@@ -5,28 +5,40 @@
 #define HEIGHT 700
 
 int iRand(int min, int max) {
-    return min + rand() % (max-min+1);
+	return min + rand() % (max - min + 1);
 }
 
 int main(int argc, char* argv[]) {
-    //vars
-    long Time = 0, T0 = 0;
-    HWND windows = init(WIDTH, HEIGHT);
-    ImageSheet tank = ImageSheet("tank.bmp", 64, 64, 1024, 128);
-    Time = TIME;
-    T0 = Time;
-    begin();
-    while(!KEY(VK_ESCAPE)) {
-        resetCol();
-        clear();
-        int angle = tank.mouseMagic(200.0,200.0);
+	//vars
+	long Time = 0, T0 = 0;
+	HWND window = init(WIDTH, HEIGHT);
+	ImageSheet tank = ImageSheet("tank.bmp", 64, 64, 1024, 128);
+	Time = TIME;
+	T0 = Time;
+	begin();
+	double posX = 200.0, posY = 200.0;
+	while (!KEY(VK_ESCAPE)) {
+		resetCol();
+		clear();
+		int angle = tank.mouseMagic(posX, posY);
 		if (angle >= 352) angle = 351;
-		tank.draw(200,200);
-        tank.draw(200,200, angle/22, 1);
-        if(KEY(VK_RIGHT)) tank.moveRight();
-        else if(KEY(VK_LEFT)) tank.moveLeft();
-        gsleep(10);
-    }
+		tank.draw(posX, posY);
+		tank.draw(posX, posY, angle / 22, 1);
+		if (TIME - Time > 0.5) {
+			if (KEY(VK_RIGHT)) {
+				tank.moveRight();
+			}
+			else if (KEY(VK_LEFT)) {
+				tank.moveLeft();
+			}
+			Time = TIME;
+		}
+		if (KEY(VK_UP)) {
+			posX += sin((tank.posX * 22.5)*M_PI / 180) * 5;
+			posY -= cos((tank.posX * 22.5)*M_PI / 180) * 5;
+		}
+		gsleep(10);
+	}
 	end();
 	return 0;
 }
